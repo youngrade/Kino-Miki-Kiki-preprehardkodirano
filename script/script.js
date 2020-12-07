9a91fd6b8ba7690665bc314e307ecd019ad56588
youngrade <katarza.21@gmail.com>
2020-12-07 02:58:52 +0100

    Globalna navigacija + subota

---
+++ script/script.js
@@ -0,0 +1,14 @@
+const meni=document.getElementById("meni").style;
+
+function nemaProjekcija(){
+    alert("Nema zakazanih projekcija za odabrani dan!");
+}
+window.addEventListener('scroll', ()=>{
+    if(this.scrollY===0){
+        meni.backgroundColor="#20232a";
+        meni.height=meniHeight;
+    }
+    else{
+        meni.backgroundColor="#20232aad";
+    }
+});
