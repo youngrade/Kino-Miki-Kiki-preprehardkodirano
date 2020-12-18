8580e4400f7c65ee9ecfc101d672814f3392124f
youngrade <katarza.21@gmail.com>
2020-12-18 02:01:52 +0100

    Napucan javascript za odabir sjedišta

---
+++ script/joker-projekcije.js
@@ -0,0 +1,90 @@
+const p1500 = `
+<div class="seats-row">
+            <div class="seat">A1</div>
+            <div class="seat">A2</div>
+            <div class="seat">A3</div>
+            <div class="seat">A4</div>
+            <div class="seat-zauzeto">A5</div>
+            <div class="seat-zauzeto">A6</div>
+            <div class="seat">A7</div>
+          </div>
+          <div class="seats-row">
+            <div class="seat-zauzeto">B1</div>
+            <div class="seat-zauzeto">B2</div>
+            <div class="seat">B3</div>
+            <div class="seat-zauzeto">B4</div>
+            <div class="seat-zauzeto">B5</div>
+            <div class="seat-zauzeto">B6</div>
+            <div class="seat">B7</div>
+          </div>
+          <div class="seats-row">
+            <div class="seat">C1</div>
+            <div class="seat-zauzeto">C2</div>
+            <div class="seat-zauzeto">C3</div>
+            <div class="seat">C4</div>
+            <div class="seat">C5</div>
+            <div class="seat">C6</div>
+            <div class="seat-zauzeto">C7</div>
+          </div>
+          <div class="seats-row">
+            <div class="seat">D1</div>
+            <div class="seat-zauzeto">D2</div>
+            <div class="seat-zauzeto">D3</div>
+            <div class="seat-zauzeto">D4</div>
+            <div class="seat-zauzeto">D5</div>
+            <div class="seat">D6</div>
+            <div class="seat-zauzeto">D7</div>
+          </div>
+          <div class="seats-row">
+            <div class="seat">E1</div>
+            <div class="seat">E2</div>
+            <div class="seat-zauzeto">E3</div>
+            <div class="seat-zauzeto">E4</div>
+            <div class="seat">E5</div>
+            <div class="seat-zauzeto">E6</div>
+            <div class="seat-zauzeto">E7</div>
+            `;
+const p2030 = `<div class="seats-row">
+            <div class="seat-zauzeto">A1</div>
+            <div class="seat-zauzeto">A2</div>
+            <div class="seat-zauzeto">A3</div>
+            <div class="seat-zauzeto">A4</div>
+            <div class="seat">A5</div>
+            <div class="seat-zauzeto">A6</div>
+            <div class="seat-zauzeto">A7</div>
+          </div>
+          <div class="seats-row">
+            <div class="seat">B1</div>
+            <div class="seat-zauzeto">B2</div>
+            <div class="seat-zauzeto">B3</div>
+            <div class="seat-zauzeto">B4</div>
+            <div class="seat-zauzeto">B5</div>
+            <div class="seat-zauzeto">B6</div>
+            <div class="seat">B7</div>
+          </div>
+          <div class="seats-row">
+            <div class="seat">C1</div>
+            <div class="seat">C2</div>
+            <div class="seat">C3</div>
+            <div class="seat-zauzeto">C4</div>
+            <div class="seat-zauzeto">C5</div>
+            <div class="seat">C6</div>
+            <div class="seat">C7</div>
+          </div>
+          <div class="seats-row">
+            <div class="seat">D1</div>
+            <div class="seat-zauzeto">D2</div>
+            <div class="seat-zauzeto">D3</div>
+            <div class="seat-zauzeto">D4</div>
+            <div class="seat-zauzeto">D5</div>
+            <div class="seat">D6</div>
+            <div class="seat">D7</div>
+          </div>
+          <div class="seats-row">
+            <div class="seat">E1</div>
+            <div class="seat">E2</div>
+            <div class="seat-zauzeto">E3</div>
+            <div class="seat-zauzeto">E4</div>
+            <div class="seat">E5</div>
+            <div class="seat-zauzeto">E6</div>
+            <div class="seat-zauzeto">E7</div>`;
--- script/kupikartu.js
+++ script/kupikartu.js
@@ -1,22 +1,74 @@
-const slobodnaSjedista = document.getElementsByClassName("seat");
+const jokerTermini = {
+  "15:00": p1500,
+  "20:30": p2030,
+};
+var slobodnaSjedista = null;
 const odabranihSjedista = document.getElementById("ukupno-sjedista");
 const zaPlatiti = document.getElementById("za-platiti-span");
+const cijenaKarte = document.getElementById("cijena-karte");
 
-for (let i = 0; i < slobodnaSjedista.length; i++) {
-  slobodnaSjedista[i].addEventListener("click", (e) => {
-    //console.log(e.target.innerHTML);
-    if (e.target.className == "seat") {
-      e.target.classList.remove("seat");
-      e.target.classList.add("seat-odabrano");
+inicijalizujSlobodnaSjedista();
+function inicijalizujSlobodnaSjedista() {
+  slobodnaSjedista = document.getElementsByClassName("seat");
 
-      odabranihSjedista.innerText++;
-      zaPlatiti.innerText = parseInt(zaPlatiti.innerText) + 5;
-    } else {
-      e.target.classList.remove("seat-odabrano");
-      e.target.classList.add("seat");
+  //Na svako slobodno sjediste dodajemo event listener na klik
+  for (let i = 0; i < slobodnaSjedista.length; i++) {
+    slobodnaSjedista[i].addEventListener("click", (e) => {
+      //console.log(e.target.innerHTML);
+      if (e.target.className == "seat") {
+        e.target.classList.remove("seat");
+        e.target.classList.add("seat-odabrano");
 
-      odabranihSjedista.innerText--;
-      zaPlatiti.innerText = parseInt(zaPlatiti.innerText) - 5;
-    }
-  });
+        odabranihSjedista.innerText++;
+        zaPlatiti.innerText =
+          parseInt(zaPlatiti.innerText) + parseInt(cijenaKarte.innerText);
+      } else {
+        e.target.classList.remove("seat-odabrano");
+        e.target.classList.add("seat");
+
+        odabranihSjedista.innerText--;
+        zaPlatiti.innerText =
+          parseInt(zaPlatiti.innerText) - parseInt(cijenaKarte.innerText);
+      }
+      provjeriDugme();
+    });
+  }
+}
+
+function ponistiOdabranaSjedista() {
+  var odabrana = document.querySelectorAll(".seat-odabrano");
+
+  for (let i = 0; i < odabrana.length; i++) {
+    odabrana[i].classList.remove("seat-odabrano");
+    odabrana[i].classList.add("seat");
+  }
+  nulirajSpanove();
+  provjeriDugme();
+}
+function odabirTermina() {
+  let termin = document.getElementById("selekcija");
+  if (termin.value == "15:00") {
+    document.getElementById("seats-all").innerHTML = jokerTermini[termin.value];
+    cijenaKarte.innerText = 5;
+  } else if (termin.value == "20:30") {
+    document.getElementById("seats-all").innerHTML = jokerTermini[termin.value];
+    cijenaKarte.innerText = 8;
+  }
+  nulirajSpanove();
+  ponistiOdabranaSjedista();
+  provjeriDugme();
+  inicijalizujSlobodnaSjedista();
+}
+function nulirajSpanove() {
+  odabranihSjedista.innerText = 0;
+  zaPlatiti.innerText = 0;
+  provjeriDugme();
+}
+function provjeriDugme() {
+  let dugme = document.querySelector("#rezervisi-btn button");
+  if (zaPlatiti.innerText == 0) {
+    dugme.id = "btn-blocked";
+  } else {
+    dugme.id = "btn-rezervisi";
+  }
 }
